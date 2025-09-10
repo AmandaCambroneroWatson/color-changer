b01d4e2a4da737654b363b242523796fa424d4e6
AmandaCambroneroWatson <amandacwatson88@gmail.com>
2025-09-10 09:25:09 +0200

    Cleaned up the script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
   const colorChanger = document.getElementById("colorChanger");
   const colors = ["HotPink", "LightPink", "Crimson"];
   const originalColor = "black";
+  const statusText = document.getElementById("status");
 
   
 let currentIndex = 0;
@@ -20,37 +21,9 @@ colorChanger.addEventListener("click", () => {
   }
 });
 
-// function randomColor(colors) {
-//   return colors[Math.floor(Math.random() * colors.length)];
-// }
+function myFunction() {
+   let element = document.body;
+   element.classList.toggle("dark-mode");
+}
 
-// colorChanger.addEventListener('click', () => {
-//    const newColor = randomColor(colors);
-
-//    document.body.style.backgroundColor = newColor;
-//    colorChanger.textContent = `Color: ${newColor}`;
-  
-//    console.log(newColor);
-// });
-
-
-// function randomColor(arr) {
-//     return arr[Math.floor(Math.random() * arr.length)];
-//   }
-
-//   const originalColor = "black";
-//   let isOriginal = true;
-
-//   colorChanger.addEventListener("click", () => {
-//     if (isOriginal) {
-//       const newColor = randomColor(colors);
-//       document.body.style.backgroundColor = newColor;
-//       colorChanger.textContent = `Color: ${newColor}`;
-//       console.log(newColor);
-//     } else {
-//       document.body.style.backgroundColor = originalColor;
-//       colorChanger.textContent = "Change Color";
-//     }
-//     isOriginal = !isOriginal;
-//   });
 
